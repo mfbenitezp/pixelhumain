0747cb703c21f6f6b94a126a9b2204df01ba3628
Alaa Bakry <alaa.cs@hotmail.com>
2019-01-23 16:32:39 +0100

    translate(automatic) loader.php

--- ph/protected/messages/es/loader.php
+++ ph/protected/messages/es/loader.php
@@ -1,16 +1,16 @@
 <?php
 	return array(
-		"Loading"=>"Chargement en cours",
-		"Thanks to be patient a moment"=>"Merci de patienter quelques instants",
-		"1 little coin to contribute"=>"1 petite pièce pour participer",
-		"Help us to forward our work" =>"Aidez-nous à poursuivre notre travail",
-		"More informations about our financial situation"=>"En savoir plus sur notre situation financière",
-		"Make a donation to the NGO"=>"Faire un don à l&#146;association",
-		"1 little coin for a great puzzle"=>"1 petite pièce pour un grand puzzle",
-		"Contribute"=>"Contribuer",
-		"Member ( 1€/month )"=>"Membre ( 1€/mois )",
-		"Sponsor ( 50€/month )"=>"Sponsor ( 50€/mois )",
-		"Make a donation"=>"Faire un don",
+		"Loading"	=>	"Cargando en progreso.",
+"Thanks to be patient a moment"	=>	"Gracias por esperar un momento.",
+"1 little coin to contribute"	=>	"1 pequeña sala para participar",
+"Help us to forward our work"	=>	"Ayúdanos a continuar nuestro trabajo.",
+"More informations about our financial situation"	=>	"Aprenda más sobre nuestra situación financiera.",
+"Make a donation to the NGO"	=>	"Haz una donación a la asociación",
+"1 little coin for a great puzzle"	=>	"Una pequeña pieza para un gran rompecabezas",
+"Contribute"	=>	"Contribuye",
+"Member ( 1€/month )"	=>	"Miembro ( €1/mes)",
+"Sponsor ( 50€/month )"	=>	"Patrocinador (50€/mes)",
+"Make a donation"	=>	"Donar",
 	);
 
 ?>
