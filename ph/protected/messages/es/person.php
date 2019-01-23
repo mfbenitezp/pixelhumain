09a57f95740c11ebb71ce3d5e9ed25a3aacc8f25
Alaa Bakry <alaa.cs@hotmail.com>
2019-01-23 15:51:24 +0100

    translate(automatic) person.php

--- ph/protected/messages/es/person.php
+++ ph/protected/messages/es/person.php
@@ -1,54 +1,47 @@
 <?php
 
 return array(
+	"Problem inserting the new person : "	=>	"Problema para insertar a la nueva persona:",
+"is missing"	=>	"está perdido",
+"Problem inserting the new person : email is not well formated"	=>	"Problema para insertar a la nueva persona: el correo electrónico no está bien formateado",
+"Problem inserting the new person : a person with this email already exists in the plateform"	=>	"Problema al insertar la nueva persona: una persona con este correo electrónico ya existe en la forma de la placa.",
+"Problem inserting the new person : a person with this username already exists in the plateform"	=>	"Problema al insertar la nueva persona: una persona con este nombre de usuario ya existe en la plataforma.",
+"Problem inserting the new person : unknown city"	=>	"Problema para insertar a la nueva persona: ciudad desconocida",
+"Find people you know by name or email"	=>	"Encuentra a la gente que conoces por nombre o correo electrónico",
+"Add a Person"	=>	"Añadir una persona",
+//Change Password
+"Your current password is incorrect"	=>	"Su contraseña actual es incorrecta",
+"The new password should be 8 caracters long"	=>	"La nueva contraseña debe tener 8 caracteres de longitud",
+"Your password has been changed with success !"	=>	"Su contraseña ha sido cambiada con éxito!",
+//Invitation process
+"Sorry, you can't invite more people to join the platform. You do not have enough invitations left."	=>	"Lo siento, no puedes invitar a más gente a unirse a la plataforma. No te quedan suficientes invitaciones.",
+"The person has been updated"	=>	"La persona ha sido actualizada",
+"Birth date"	=>	"Fecha de nacimiento",
+"Phone"	=>	"Teléfono",
+"Mobile"	=>	"Móvil",
+"Country"	=>	"Country",
+"Postal Code"	=>	"Código Postal",
+"Address"	=>	"Dirección",
+"Fax"	=>	"Fax",
+"Username"	=>	"Nombre de usuario",
+"Short Description"	=>	"Breve descripción",
+"Contact information"	=>	"Información de contacto",
+"My space"	=>	"Mi espacio",
+"My city"	=>	"Mi ciudad",
+"My Votes / Discussions"	=>	"Mis Votos / Discusiones",
+"Invite someone"	=>	"Invita a alguien",
+"Create an event"	=>	"Crear un evento",
+"Create a project"	=>	"Crear un proyecto",
+"Create an organization"	=>	"Crear una organización",
+"Sign out"	=>	"Cerrar sesión",
+"Edit your informations"	=>	"Edite sus datos",
+"Enter your name"	=>	"Introduzca su nombre",
+"Enter your user name"	=>	"Introduzca su nombre de usuario",
+"Enter your phones"	=>	"Ingresen sus teléfonos",
+"Enter your mobiles"	=>	"Entren en sus móviles",
+"Enter your fax"	=>	"Ingresa tu fax",
+"Hello, \\nCome and meet me on that website!\\nAn email, your town and you are connected to your city!\\nYou can see everything that happens in your city and act for the commons."	=>	"Hola, ven y reúnete conmigo en esa página web! Un email, tu ciudad y tu estás conectado a tu ciudad! Puedes ver todo lo que pasa en tu ciudad y actuar por los comunes."
 
-	'Problem inserting the new person : ' => "Problème à l'insertion du nouvel utilisateur : ",
-	'is missing' => 'est manquant',
-	'Problem inserting the new person : email is not well formated' => "Problème à l'insertion du nouvel utilisateur : l'email n'est pas bien formaté",
-	"Problem inserting the new person : a person with this email already exists in the plateform" => "Problème à l'insertion du nouvel utilisateur : une personne avec cet mail existe déjà sur la plateforme",
-	"Problem inserting the new person : a person with this username already exists in the plateform" => "Problème à l'insertion du nouvel utilisateur : une personne avec ce username existe déjà sur la plateforme",
-	"Problem inserting the new person : unknown city" => "Problème à l'insertion du nouvel utilisateur : la ville est inconnue",
-	'Find people you know by name or email' => 'Trouvez quelqu&#146;un que vous connaissez grace à son nom ou son e-mail',
-	'Add a Person' => 'Inviter quelqu&#146;un',
-	//Change Password
-	"Your current password is incorrect" => "L'ancien mot de mot de passe est incorrect.",
-	"The new password should be 8 caracters long" => "Le nouveau mot de passe doit faire au moins 8 caractères de long.",
-	"Your password has been changed with success !" => "Votre mot de passe a été changé avec succès.",
-	//Invitation process
-	"Sorry, you can't invite more people to join the platform. You do not have enough invitations left." => "Désolé mais vous ne pouvez pas inviter plus de personne à rejoindre la plateform. Vous n'avez pas assez d'invitations",
-	"The person has been updated" => "Mise à jour du citoyen réussie",
-
-	"Birth date" => "Date de naissance",
-	"Phone" => "Fixe",
-	"Mobile" => "Mobile",
-	"Country" => "Pays",
-	"Postal Code" => "Code postal",
-	"Address" => "Adresse",
-	"Fax" => "Fax",
-	"Username" => "Nom d'utilisateur",
-	"Short Description" => "Description courte",
-	"Contact information" => "Informations générales",
-
-	"My space" => "Mon espace perso",
-	"My city" => "Ma commune",
-	"My Votes / Discussions" => "Mes Votes / Discussions",
-	"Invite someone" => "Inviter quelqu'un",
-	"Create an event" => "Créer un événement",
-	"Create a project" => "Créer un projet",
-	"Create an organization" => "Référencer une organisation",
-	"Sign out" => "Déconnexion",
-	"Edit your informations" => "Editer vos informations",
-
-
-	"Enter your name" => "Entrer votre nom et prénom",
-	"Enter your user name" => "Entrer votre nom d'utilisateur",
-
-	"Enter your phones" => "Saisir vos numéros de téléphones fixes, séparer les numéros par une virgule.",
-	"Enter your mobiles" => "Saisir vos numéros de téléphones portables, séparer les numéros par une virgule.",
-	"Enter your fax" => "Saisir vos numéros de fax, séparer les numéros par une virgule.",
-
-	"Hello, \\nCome and meet me on that website!\\nAn email, your town and you are connected to your city!\\nYou can see everything that happens in your city and act for the commons." => "Bonjour, \\nViens me rejoindre sur ce site ! \\nUn email, un code postal et tu es communecté. Tu peux voir tout ce qui se passe dans ta commune et agir pour le bien commun.\\n"
-	
 );
 
 ?>
